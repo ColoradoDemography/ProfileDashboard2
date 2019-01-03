fb517fb054a244ad5606e159c102fe18bcc0d085
ABSOD <32849428+ABSOD@users.noreply.github.com>
2019-01-03 09:59:15 -0700

    Update dataL.js

--- www/dataL.js
+++ www/dataL.js
@@ -69,5 +69,13 @@ $(document).on('shiny:inputchanged', function(event) {
 
 	
   }	
- 	   
+ if (event.name == 'outputPDF') {
+      var dl1 = "window.dataLayer = window.dataLayer || [];"
+      var dl2 = " window.dataLayer.push({ event: 'DownloadReport',
+            " 'DownloadReport' : yes;"	
+      var newScript = document.createElement("script");
+      var inlineScript = document.createTextNode(dl1 + dl2);
+       newScript.appendChild(inlineScript); 
+       target.appendChild(newScript);	
+	  }	   	   
   });
