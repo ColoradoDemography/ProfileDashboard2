18fd39ac64f7b44bfddb1a4877196cb3aa171e29
ABSOD <32849428+ABSOD@users.noreply.github.com>
2019-03-14 16:38:27 -0600

    Update app.R

--- app.R
+++ app.R
@@ -869,14 +869,14 @@ server <- function(input, output, session) {
           
           
           poph3.info <- tags$div(boxContent(title= "Housing Units by Type",
-                                            description= "The Owner-Occupied Housing Table displays the characteristics of owner-occupied housing in a selected place.",
+                                            description= "The Housing Unit Table displays the characteristics of owner-occupied and rental housing in a selected place.",
                                             MSA= "F", stats = "F", muni = "F", multiCty = idList$multiCty, PlFilter = "F", 
                                             urlList = list(c("American Community Survey American Fact Finder, Series B25010, B25032, B25033, and B25037","https://factfinder.census.gov/faces/nav/jsf/pages/index.xhtml")) ),
                                  tags$br(),
                                  downloadObjUI("poph3tabl"),downloadObjUI("poph3data"))
           
           poph4.info <- tags$div(boxContent(title= "Comparative Housing Values",
-                                            description= "The Comparative Housing Table compares the economic characteristics of  owner-occupied and rental housing in a selected place to the State.",
+                                            description= "The Comparative Housing Table compares the cost of  owner-occupied and rental housing in a selected place to the State.",
                                             MSA= "F", stats = "F", muni = "F", multiCty = idList$multiCty, PlFilter = "F", 
                                             urlList = list(c("American Community Survey American Fact Finder, Series B25077 and B25092","https://factfinder.census.gov/faces/nav/jsf/pages/index.xhtml")) ),
                                  tags$br(),
