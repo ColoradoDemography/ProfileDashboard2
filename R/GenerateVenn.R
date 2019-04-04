02469312f5eb2bb0a06fcfdf6d1dc70410e79b5b
ABSOD <32849428+ABSOD@users.noreply.github.com>
2019-04-04 11:07:42 -0500

    Transition to Eulerr

--- R/GenerateVenn.R
+++ R/GenerateVenn.R
@@ -7,7 +7,7 @@
 GenerateVenn <- function(DBPool,listID){
 
   # Collecting place ids from  idList, setting default values
-  
+
   ctyfips <- listID$ctyNum
   ctyname <- listID$ctyName
   placefips <- listID$plNum
@@ -17,132 +17,76 @@ GenerateVenn <- function(DBPool,listID){
     placename <- ""
   }
   
-  options(warn=-1)  # Suppressing warning messages produced by VennDiagram
-
 if(nchar(placefips) != 0) {
   sumSQL <- paste0("SELECT * FROM data.otm_place_summary WHERE fips = '",placefips,"' ;")
   placeSQL <- paste0("SELECT * FROM data.otm_place_place WHERE fips = '",placefips,"' ;")
-  
-  f.summary <- dbGetQuery(DBPool, sumSQL)
-  f.place <- dbGetQuery(DBPool, placeSQL)
-  location <- paste0(placename,"\n","All Jobs, ",as.character(f.summary$year))
-  
-  lin_wout <- as.numeric(f.summary$livein_workout)
-  lout_win <- as.numeric(f.summary$workin_liveout)
-  lin_win <-  as.numeric(f.summary$livein_workin)
-  
-  region1 <- lout_win + lin_win #Live outside, work in
-  region2 <- lin_wout + lin_win #Live in, work outside
-  
-  crossRegion <- lin_win 
-  
-  
-  # By default, VennDiagram outputs the larger Region value in the left hand postion.
-  # This code block insures that the diagram is correct
-  
-  if(lin_wout <= lout_win){
-    diag <- draw.pairwise.venn(region1, region2, crossRegion, inverted = TRUE,
-                               lty = rep("solid", 2), cat.col = rep("black", 2),
-                               cex = 1, cat.cex = 1, cat.default.pos= "text", ext.text = FALSE,
-                               fill = c("chartreuse4", "aquamarine2"), alpha = rep(0.5, 2),
-                               euler.d=TRUE,scaled=TRUE, ind = FALSE, print.mode="raw")
-  } else{
-    diag <- draw.pairwise.venn(region1, region2, crossRegion, inverted = FALSE,
-                               lty = rep("solid", 2), cat.col = rep("black", 2),
-                               cex = 1, cat.cex = 1,  cat.default.pos= "text", ext.text  = FALSE,
-                               fill = c("chartreuse4", "aquamarine2"), alpha = rep(0.5, 2),
-                               euler.d=TRUE,scaled=TRUE, ind = FALSE, print.mode="raw")
-  }
 } else {
   sumSQL <- paste0("SELECT * FROM data.otm_county_summary WHERE fips = '",ctyfips,"' ;")
   placeSQL <- paste0("SELECT * FROM data.otm_county_place WHERE fips = '",ctyfips,"' ;")
-  
+}
+  # reading Data 
   f.summary <- dbGetQuery(DBPool, sumSQL)
   f.place <- dbGetQuery(DBPool, placeSQL)
-  
-  location <- paste0(ctyname,"\n","All Jobs, ",as.character(f.summary$year))
   
   lin_wout <- as.numeric(f.summary$workin_liveout)
   lout_win <- as.numeric(f.summary$livein_workout)
   lin_win <-  as.numeric(f.summary$livein_workin)
   
-  region1 <- lout_win + lin_win #Live outside, work in
-  region2 <- lin_wout + lin_win #Live in, work outside
+
+  rawVenn <- euler(c("A" = lout_win, "B" = lin_wout, "A&B" = lin_win ))
+  cols <- c("lightblue1", "lightyellow1","olivedrab1")
   
-  crossRegion <- lin_win 
   
-  if(lin_wout >= lout_win){
-    diag <- draw.pairwise.venn(region1, region2, crossRegion, inverted = TRUE,
-                               lty = rep("solid", 2), cat.col = rep("black", 2),
-                               cex = 1, cat.cex = 1, cat.default.pos= "text", ext.text = FALSE,
-                               fill = c("chartreuse4", "aquamarine2"), alpha = rep(0.5, 2),
-                               euler.d=TRUE,scaled=TRUE, ind = FALSE, print.mode="raw")
-  } else{
-    diag <- draw.pairwise.venn(region1, region2, crossRegion, inverted = FALSE,
-                               lty = rep("solid", 2), cat.col = rep("black", 2),
-                               cex = 1, cat.cex = 1,  cat.default.pos= "text", ext.text  = FALSE,
-                               fill = c("chartreuse4", "aquamarine2"), alpha = rep(0.5, 2),
-                               euler.d=TRUE,scaled=TRUE, ind = FALSE, print.mode="raw")
-  }
-}
+  Vdiag <- plot(rawVenn,
+                quantities = format(rawVenn$original.values, big.mark = ",",scientific = FALSE),
+                fill = cols,
+                fill_opacity = 0.5, border = "black",
+                labels=FALSE,
+                counts = TRUE)
   
-
-
-  # Formatting the labels for the output diagram
-  # Change labels for first three text grobs
-  # hard-coded three, but it would be the number of text labels
-  # minus the number of groups passed to venn.diagram
-
- 
-  idx <- sapply(diag, function(i) grepl("text", i$name))  # Identifying the text grobs
-
-  for(i in 1:3){
-    diag[idx][[i]]$label <-
-      format(as.numeric(diag[idx][[i]]$label), big.mark=",", scientific=FALSE)
-    if(diag[idx][[i]]$label == "NA") {
-      diag[idx][[i]]$label <- ""
-    }
-  } #End I Loop
-
-
   #Building Legend
   
   if(nchar(placefips) != 0) {
     legstr1 <- paste0("Employees in ",placename," living elsewhere")
     legstr2 <- paste0("Residents of ",placename," working elsewhere")
     legstr3 <- paste0("Employed and Live in ",placename)
+    plot_title <- paste0(placename,": All Jobs, 2015")
   } else {
     legstr1 <- paste0("Employees in ",ctyname," living elsewhere")
     legstr2 <- paste0("Residents of ",ctyname," working elsewhere")
     legstr3 <- paste0("Employed and Live in ",ctyname)
+    plot_title <- paste0(ctyname,": All Jobs, 2015")
   }
   
-  cols <- c("chartreuse4", "aquamarine2","aquamarine3")
+  
   lg <- legendGrob(labels=c(legstr1,
                             legstr2,
                             legstr3),
                    pch=rep(19,length(c(legstr1,
                                        legstr2,
                                        legstr3))),
-
+                   
                    gp=gpar(col=cols, fill="gray", fontsize=12),
                    byrow=TRUE)
-
-  g <- gTree(children = gList(diag))
-
+  
+  
+  g <- gTree(children = gList(Vdiag))
 
   #outVenn is the final VennDiagram
   #Formatting citation
-  sub.label = textGrob(captionSrc("LODES",""),
+  captionGrob = textGrob(captionSrc("LODES",""),
                        gp=gpar(fontsize=12),
                        x = unit(1, "npc"),
                        hjust = 1,
                        vjust = 0)
+  titleGrob <- textGrob(plot_title, gp=gpar(fontsize=12,fontface="bold"))
+  
+  gridvenn <- arrangeGrob(titleGrob, g, lg, captionGrob, nrow=4, heights=unit(c(1,2,1,1),"in"))
+  
+  
+  
+  outVenn <- as.ggplot(gridvenn)
 
-    outVenn <- arrangeGrob(g, lg, nrow=3, ncol=1, heights=c(4,1,2),
-                         top=textGrob(location, gp=gpar(fontsize=15,font=8)), sub=sub.label)
-
-  options(warn=0)  # restoring Warning Messages
   # Finalizing the output data sets
   #selecting the top 10 places
 
