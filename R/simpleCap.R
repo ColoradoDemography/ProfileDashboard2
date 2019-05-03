053404257112fde210fce505dfffe558231c65d5
ABSOD <32849428+ABSOD@users.noreply.github.com>
2019-05-03 15:02:41 -0600

    Update simpleCap.R

--- R/simpleCap.R
+++ R/simpleCap.R
@@ -5,6 +5,7 @@
 #' @param x input string
 #' @return String formatted in Proper Case
 #' @export
+
 simpleCap <- function(x) {
   s <- strsplit(x, " ")[[1]]
   paste(toupper(substring(s, 1,1)), tolower(substring(s, 2)),
