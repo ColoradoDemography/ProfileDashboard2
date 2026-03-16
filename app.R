5caebf461410564e42278fd5c96b454505ddb80f
ABSOD <32849428+ABSOD@users.noreply.github.com>
2026-03-16 08:59:46 -0600

    Update app.R

--- app.R
+++ app.R
@@ -107,8 +107,8 @@ library('config')
  tagManJS <- "/srv/shiny-server/ProfileDashboard2/www/tag_manager.js"
 
 # Current ACS database
-curACS <- "acs1923"
-curYr <- 2023
+curACS <- "acs2024"
+curYr <- 2024
 fipslist <<- ""
 
 # Set up database pool 1/23/19
